1dc6bc114830761d0f3f20eff6b616750a28b9f5
Fajri Abdillah <clasense4@gmail.com>
2020-05-12 20:26:49 +0700

    Update CORS

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
